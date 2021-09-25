b1d4a1f27fbb8c5a3a1e7239554fa351914009bd
ListiaNingrum <listianngrm02@gmail.com>
2021-09-25 14:06:07 +0700

    tugas pertemuan 2

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,20 +32,18 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/hello', to:'Selamatdatang::ha;_awal');
 
 /*
  * --------------------------------------------------------------------
- * Additional Routing
+ * Route Definitions
  * --------------------------------------------------------------------
- *
- * There will often be times that you need additional routing and you
- * need it to be able to override any defaults in this file. Environment
- * based routes is one such time. require() additional route files here
- * to make that happen.
- *
- * You will have access to the $routes object within that file without
- * needing to reload it.
  */
-if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
-    require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
-}
+ 
+
+// we get a perfomance increase by specifying the dafault
+// route since we don't have scan directories
+$routes->get( from: '/', to: 'home::index');
+$routes->get( from: '/hello', to: 'SelamatDatang::hal_awal');
+$routes->get( from: '/login', to: 'SelamatDatang::beranda_login');
+$routes->get( from: '/daftar-member', to: 'SelamatDatang::daftar_member');
---
+++ app/Views/halaman/login.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>perpustakaan login</title>
+    </head>
+    <body>
+
+        <h1>selamat datang di Sistem Informasi Perpustakaan<h1>
+        <P>
+            Ini addalah layanan perpustakaan digital masa kini
+            <a href="<?=base_url( relativePath: '/daftar-member')?>">klik daftar</a>
+        </p>
+
+    </body>
+</html>
