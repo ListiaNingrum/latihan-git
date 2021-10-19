f8a7d66333404276dd55be13ea7b7e945016c835
ListiaNingrum <90951689+ListiaNingrum@users.noreply.github.com>
2021-10-19 10:56:50 +0700

    Update 2021-10-09-062144_PenggunaMigration_12200765.php

--- app/Database/Migrations/2021-10-09-062144_PenggunaMigration_12200765.php
+++ app/Database/Migrations/2021-10-09-062144_PenggunaMigration_12200765.php
@@ -4,31 +4,33 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class Pengguna extends Migration
+class PenggunaMigration extends Migration
 {
     public function up()
     {
-        $this->forge->addfield([
+        //
+        $this->forge->addField([
             'id' => [
-                'type'               => 'INT', 'constraint' => 4,
-                'unsigned'           => true,
-                'auto_increment'     => true
+                'type'            => 'INT', 'constraint' => 4,
+                'unsigned'        => true,
+                'auto_increment'  => true
             ],
             'nama' => [
-                'type'             => 'VARCHAR', 'constraint' => 50,
-                'null'             => false
+                'type'            => 'VARCHAR', 'constraint' => 50,
+                'null'            => false
             ],
             'password' => [
-                'type'             => 'VARCHAR', 'constraint' => 32,
-                'null'             => false
+                'type'            => 'VARCHAR', 'constraint' => 32,
+                'null'            => false
             ]
         ]);
         $this->forge->addKey('id', true);
-        $this->forge->createTable('pengguna');
+        $this->forge->createTable('pengguna_12200921');
     }
 
     public function down()
     {
-        $this->forge->dropTable('pengguna');
+        //
+        $this->forge->dropTable('pengguna_12200921');
     }
 }
